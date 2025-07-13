b3814ea837af7310515697b00fa08a3df466a5e1
Franklin Ramos <franklinramos@outlook.com>
2025-07-13 15:00:35 -0300

    Adiciona log de depuração na função getUser para exibir a senha recebida

--- src/utils/actions/get-user.ts
+++ src/utils/actions/get-user.ts
@@ -3,6 +3,7 @@
 import { User } from '@/types/user'
 
 export async function getUser(email: string, password: string): Promise<User> {
+  console.log('🚀 ~ getUser ~ password:', password)
   return {
     id: '4c8db753',
     name: 'test user',
